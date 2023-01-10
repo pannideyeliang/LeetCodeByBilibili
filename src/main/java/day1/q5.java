126d35dcea511aa4ed8cfef9405ff9aad7def9c8
chenjiale <2601823097@qq.com>
2023-01-10 23:23:37 +0800

    q6

--- src/main/java/day1/q5.java
+++ src/main/java/day1/q5.java
@@ -1,5 +1,7 @@
 package day1;
 
+import javax.tools.ForwardingFileObject;
+
 /**
  * @author 陈佳乐
  * @create 2023-01-10-14:04
@@ -7,7 +9,7 @@ package day1;
 public class q5 {
     //5. 最长回文子串
     public static void main(String[] args) {
-        System.out.println(new Solution5().longestPalindrome("abb"));
+        System.out.println(new Solution5().longestPalindrome("abcdcba"));
     }
 }
 
@@ -40,10 +42,45 @@ class Solution5 {
         return s.substring(left + 1, right);
     }
 
-    //    2.采用万能方法，动态规划
+    //    2.采用万能方法，动态规划,动态转移方程实际上就是推导方程
     public String longestPalindromeV2(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
 
-        return null;
+        int maxLen = 1;
+        int begin = 0;
+//        动态数组，用于存储从i 到 j 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        char[] chars = s.toCharArray();
+//        初始化，所有长度为1的子串都是回文串
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+//       开始进行动态规划的推导表达式
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (chars[j] != chars[i]) {
+                    dp[j][i] = false;
+                } else {
+                    if (i - j < 3) {
+                        dp[j][i] = true;
+                    } else {
+                        dp[j][i] = dp[j + 1][i - 1];
+                    }
+                }
+
+//                只要dp[j][i] == true，就表示是回文串，所以要记录下回文串的长度和起始位置
+                if (dp[j][i] && i - j + 1 > maxLen) {
+                    maxLen = i - j + 1;
+                    begin = j;
+                }
+            }
+
+        }
+        return s.substring(begin, begin + maxLen);
     }
 }
 
---
+++ src/main/java/day1/q6.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class q6 {
+//    6. Z 字形变换
+}
+
+
+class Solution6 {
+
+    //        普通方法
+    public String convert(String s, int numRows) {
+        if (s.length() == 2) {
+            return s;
+        }
+
+
+        return null;
+    }
+}
