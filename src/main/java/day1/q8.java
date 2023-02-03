dee749fbb2819301e3fd470fcb05c4023f93517a
chenjiale <2601823097@qq.com>
2023-02-03 13:48:02 +0800

    q8

--- src/main/java/day1/q8.java
+++ src/main/java/day1/q8.java
@@ -7,21 +7,44 @@ package day1;
 public class q8 {
     //    8. 字符串转换整数 (atoi)
     public static void main(String[] args) {
-        System.out.println(new Solution8().myAtoi("4193 with words"));
+        System.out.println(new Solution8().myAtoi("+-12"));
     }
 }
 
 class Solution8 {
     public int myAtoi(String s) {
-        StringBuilder res = new StringBuilder();
+        int res = 0;
         char[] chars = s.toCharArray();
-        for (char c : chars) {
-            if (c == '-' || c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' ||
-                    c == '6' || c == '7' || c == '8' || c == '9') {
-                res.append(c);
+        int length = s.length();
+        int temp = 0;
+        while (temp < length && chars[temp] == ' ') {
+            temp++;
+        }
+        int index = temp;
+        int flag = 1;
+        while (index < length) {
+            if (index == temp && chars[index] == '-') {
+                flag = -1;
+                index++;
+            } else if (index == temp && chars[index] == '+') {
+                flag = 1;
+                index++;
+            } else if (Character.isDigit(chars[index])) {
+                int num = chars[index] - '0';
+                if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && num > Integer.MAX_VALUE % 10)) {
+                    return Integer.MAX_VALUE;
+                }
+
+                if (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && -num < Integer.MIN_VALUE % 10)) {
+                    return Integer.MIN_VALUE;
+                }
+                res = res * 10 + flag * num;
+                index++;
+            } else {
+                break;
             }
         }
-
-        return Integer.parseInt(res.toString());
+        if (index == 0) return 0;
+        return res;
     }
 }
