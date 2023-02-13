ad0f0e4c82ab6d78ddcfb9204f42adb0a3ee9191
chenjiale <2601823097@qq.com>
2023-02-13 11:24:56 +0800

    q11，增加大佬们的写法

--- src/main/java/day2/q11.java
+++ src/main/java/day2/q11.java
@@ -7,8 +7,8 @@ package day2;
 public class q11 {
     //    11. 盛最多水的容器
     public static void main(String[] args) {
-        int[] data = new int[]{1, 1};
-        System.out.println(new Solution11().maxAreaV2(data));
+        int[] data = new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(new Solution11().maxAreaV3(data));
     }
 }
 
@@ -27,14 +27,34 @@ class Solution11 {
         return max;
     }
 
-    //    优化
+    //    优化失败，还是超出时间限制
     public int maxAreaV2(int[] height) {
         int max = 0;
-        int width = 1;
+        int maxHeight = 0;
         for (int i = 0; i < height.length - 1; i++) {
-
+            for (int j = i + 1; j < height.length; j++) {
+                if (height[j] >= height[i]) {
+                    max = Math.max(max, height[i] * (j - i));
+                } else {
+                    maxHeight = Math.min(height[i], height[j]);
+                    max = Math.max(max, maxHeight * (j - i));
+                }
+            }
         }
         return max;
     }
 
+    //    查看大佬们的代码, 采用双指针方法，从两边开始移动，长板向内移动，面积一定减小，短板向内移动，面积不一定减小,所以每次只要
+//    考虑让短的移动就好了
+    public int maxAreaV3(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int max = 0;
+        while (left < right) {
+//            每次只移动短板的情况，直至遍历所有
+            max = height[left] < height[right] ?
+                    Math.max(max, (right- left)*height[left++]):Math.max(max, (right- left)*height[right--]);
+        }
+        return max;
+    }
 }
