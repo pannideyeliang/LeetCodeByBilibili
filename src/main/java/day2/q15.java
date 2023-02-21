a72e7bc7a7a019db905166876a2a6a480a26bb84
chenjiale <2601823097@qq.com>
2023-02-21 20:20:56 +0800

    q15

---
+++ src/main/java/day2/q15.java
@@ -0,0 +1,21 @@
+package day2;
+
+import java.util.List;
+
+/**
+ * @author 陈佳乐
+ * @create 2023-02-21-15:31
+ */
+public class q15 {
+    //    15. 三数之和
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution15 {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        return null;
+    }
+}
