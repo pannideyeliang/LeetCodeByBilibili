1d4224b3cd172d5bd4163d00f2561559d54b2ca8
chenjiale <2601823097@qq.com>
2023-03-15 18:16:15 +0800

    q15 大佬代码

--- src/main/java/day2/q15.java
+++ src/main/java/day2/q15.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class q15 {
     //    15. 三数之和
     public static void main(String[] args) {
-        int[] ints = new int[]{-1,1,0,-2,1,1};
+        int[] ints = new int[]{-1, 1, 0, -2, 1, 1};
         List<List<Integer>> lists = new Solution15().threeSum(ints);
         System.out.println(lists);
     }
@@ -77,6 +77,49 @@ class Solution15 {
             }
         }
 
+        return res;
+    }
+
+    //    跟随大佬的脚步，大佬们的写法
+//    思想同上，但是代码优化
+    public List<List<Integer>> threeSumV2(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        int len = nums.length;
+        if (nums == null || len < 3) return res;
+
+//        将数组进行排序
+        Arrays.sort(nums);
+//        遍历数组中的每一个元素
+        for (int i = 0; i < len; i++) {
+//            因为是排序好的，所以如果遍历到的元素已经大于0了的
+//            话，就直接退出就好了
+            if (nums[i] > 0) break;
+//            去重，当起始的值等于前一个元素，那么得到的结果就会和前一次相同
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+//            开始进行双指针判定
+            int l = i + 1;
+            int r = len - 1;
+//            当   l 不等于  r  的时候就继续遍历
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+//                    同样进行去重，
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+//                    去重过后将左指针右移，右指针左移
+//                    这种情况是让原来的被使用的 l 和 r 不会再被使用
+                    l++;
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else if (sum > 0) {
+                    r--;
+                }
+            }
+        }
         return res;
     }
 }
