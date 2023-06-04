52e6c3a49da9d16dc206940206106c02476b11a4
chenjiale <2601823097@qq.com>
2023-06-04 22:33:46 +0800

    21. 合并两个有序链表

---
+++ src/main/java/easy/q21.java
@@ -0,0 +1,72 @@
+package easy;
+
+public class q21 {
+    //    21. 合并两个有序链表
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode head = new ListNode();
+        ListNode temp = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+                temp = temp.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+                temp = temp.next;
+            }
+        }
+        if (list1 == null) {
+            temp.next = list2;
+        }
+        if (list2 == null) {
+            temp.next = list1;
+        }
+        return head.next;
+    }
+
+    //    下面可以采用大佬的递归的思想，我们只需要关注当前需要做什么，其他的交给下一层；
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        }
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists2(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists2(list2.next, list1);
+            return list2;
+        }
+    }
+
+}
+
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
