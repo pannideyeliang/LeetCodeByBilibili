9ea07b5b68823dc5f48c157293d835b557fefa47
chenjiale <2601823097@qq.com>
2023-06-04 21:33:17 +0800

    //    20. 有效的括号

---
+++ src/main/java/easy/q20.java
@@ -0,0 +1,58 @@
+package easy;
+
+import java.util.Stack;
+
+public class q20 {
+    //    20. 有效的括号
+//    给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+//    有效字符串需满足：
+//    左括号必须用相同类型的右括号闭合。
+//    左括号必须以正确的顺序闭合。
+//    每个右括号都有一个对应的相同类型的左括号。
+    public static void main(String[] args) {
+        System.out.println(new Solution20().isValid("){"));
+    }
+}
+
+
+class Solution20 {
+    public boolean isValid(String s) {
+        if (s == null || s.equals("") || s.length() % 2 != 0) return false;
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char c : chars) {
+            if (c == '{' || c == '(' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    //    大佬们更简洁的写法
+    public boolean isValid2(String s) {
+        if (s.isEmpty())
+            return true;
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.empty() || c != stack.pop())
+                return false;
+        }
+        if (stack.empty())
+            return true;
+        return false;
+    }
+}
