54761a0041d434a87fe27dce8a54a59141b8404b
chenjiale <2601823097@qq.com>
2023-06-12 23:04:36 +0800

    //    104. 二叉树的最大深度

---
+++ src/main/java/easy/q104.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class q104 {
+    //    104. 二叉树的最大深度
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution104 {
+    //    简单，找到最深的就好了
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
