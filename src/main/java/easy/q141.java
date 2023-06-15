9b511d44acbb9b80391f3dc2bba2f9ad0a152358
chenjiale <2601823097@qq.com>
2023-06-15 22:30:31 +0800

    //    141. 环形链表

---
+++ src/main/java/easy/q141.java
@@ -0,0 +1,19 @@
+package easy;
+
+/**
+ * @author 陈佳乐
+ * @create 2023-06-15-22:24
+ */
+public class q141 {
+    //    141. 环形链表
+    public static void main(String[] args) {
+
+    }
+}
+
+public class Solution141 {
+    public boolean hasCycle(ListNode head) {
+
+        return true;
+    }
+}
