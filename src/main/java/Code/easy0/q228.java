b64d59bbb51bec116b11dd0ecc120b6129f6aa54
chenjiale <2601823097@qq.com>
2023-06-26 22:38:25 +0800

    //    228. 汇总区间

--- src/main/java/Code/easy0/q228.java
+++ src/main/java/Code/easy0/q228.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class q228 {
     //    228. 汇总区间
     public static void main(String[] args) {
+        int num[] = new int[]{0, 1, 2, 4, 5, 7};
+        new Solution228().summaryRanges(num);
 
     }
 }
@@ -13,22 +15,52 @@ public class q228 {
 
 class Solution228 {
     //    注意条件，升序
+//    有点问题，代码
     public List<String> summaryRanges(int[] nums) {
         ArrayList<String> res = new ArrayList<>();
         if (nums.length == 0) return res;
+        if (nums.length == 1) {
+            res.add(String.valueOf(nums[0]));
+            return res;
+        }
 
-        int start = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == nums[start] + i - start) {
-                continue;
-            } else {
-                if (i - start == 1) {
-                    res.add(String.valueOf(nums[i]));
-                } else {
-                    res.add(start + "->" + (i - 1));
-                    start = i;
-                }
+        int start = 0, index = 1;
+
+        while (index < nums.length) {
+            while (nums[index] - nums[index - 1] == 1) {
+                index++;
             }
+            if (start == index - 1) {
+                res.add(String.valueOf(nums[start]));
+                start = index;
+            } else {
+                res.add(nums[start] + "->" + nums[index - 1]);
+                start = index;
+            }
+            index++;
+        }
+        if (index - start == 1) res.add(String.valueOf(nums[start]));
+        return res;
+    }
+
+    //    直接查看大佬的代码，双指针
+    public List<String> summaryRanges1(int[] nums) {
+        ArrayList<String> res = new ArrayList<>();
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            int low = i;
+            i++;
+            while (i < n && nums[i] - nums[i - 1] == 1) {
+                i++;
+            }
+            int high = i - 1;
+            StringBuffer temp = new StringBuffer(Integer.toString(nums[low]));
+            if (low < high) {
+                temp.append("->");
+                temp.append(Integer.toString(nums[high]));
+            }
+            res.add(temp.toString());
         }
         return res;
     }
