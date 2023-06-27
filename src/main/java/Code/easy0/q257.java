780a5893d524b6e8c96e1eabc47dbf626b9fa3e1
chenjiale <2601823097@qq.com>
2023-06-27 23:10:37 +0800

    //    257. 二叉树的所有路径,但变量在外部

--- src/main/java/Code/easy0/q257.java
+++ src/main/java/Code/easy0/q257.java
@@ -16,22 +16,21 @@ class Solution257 {
     ArrayList<String> res = new ArrayList<>();
 
     public List<String> binaryTreePaths(TreeNode root) {
-        constructPaths(root, "", res);
+        constructPaths(root, "");
         return res;
     }
 
-    public void constructPaths(TreeNode root, String s, List<String> res) {
+    public void constructPaths(TreeNode root, String s) {
         if (root != null) {
             StringBuilder pathSB = new StringBuilder(s);
-            pathSB.append(Integer.toString(root.val));
+            pathSB.append(root.val);
             if (root.left == null && root.right == null) {  // 当前节点是叶子节点
                 res.add(pathSB.toString());  // 把路径加入到答案中
             } else {
                 pathSB.append("->");  // 当前节点不是叶子节点，继续递归遍历
-                constructPaths(root.left, pathSB.toString(), res);
-                constructPaths(root.right, pathSB.toString(), res);
+                constructPaths(root.left, pathSB.toString());
+                constructPaths(root.right, pathSB.toString());
             }
         }
-
     }
 }
