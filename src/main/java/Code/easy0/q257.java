04982e2c681b3bce05258e8e152f324db58a429e
chenjiale <2601823097@qq.com>
2023-06-27 23:08:35 +0800

    //    257. 二叉树的所有路径

---
+++ src/main/java/Code/easy0/q257.java
@@ -0,0 +1,37 @@
+package Code.easy0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class q257 {
+    //    257. 二叉树的所有路径
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Solution257 {
+    //    显然是递归
+    ArrayList<String> res = new ArrayList<>();
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        constructPaths(root, "", res);
+        return res;
+    }
+
+    public void constructPaths(TreeNode root, String s, List<String> res) {
+        if (root != null) {
+            StringBuilder pathSB = new StringBuilder(s);
+            pathSB.append(Integer.toString(root.val));
+            if (root.left == null && root.right == null) {  // 当前节点是叶子节点
+                res.add(pathSB.toString());  // 把路径加入到答案中
+            } else {
+                pathSB.append("->");  // 当前节点不是叶子节点，继续递归遍历
+                constructPaths(root.left, pathSB.toString(), res);
+                constructPaths(root.right, pathSB.toString(), res);
+            }
+        }
+
+    }
+}
