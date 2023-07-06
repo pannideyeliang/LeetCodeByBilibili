d22bfd92b345b7e856b56c0b08948f70c76db585
chenjiale <2601823097@qq.com>
2023-07-06 23:07:29 +0800

    //   53. 最大子数组和

--- src/main/java/Code/middle/q53.java
+++ src/main/java/Code/middle/q53.java
@@ -12,7 +12,9 @@ class Solution53 {
     //    可以采用动态规划
     public int maxSubArray(int[] nums) {
 //返回其最大和。
-//        dp[i]表示到达i的最大值是多少
+//        dp[i]表示以i结尾的子数组的最大值是多少
+//        所以记录前i个最大和的子序列没有意义，要是不以第i个结尾，加上第n个元素之后也不是连续子序列。
+//        答案肯定在这里
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
