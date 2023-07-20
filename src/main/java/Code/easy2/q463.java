a6959448bd79f42fb6723a0bb04dc6a71e072179
chenjiale <2601823097@qq.com>
2023-07-20 21:53:21 +0800

    //    463. 岛屿的周长

--- src/main/java/Code/easy2/q463.java
+++ src/main/java/Code/easy2/q463.java
@@ -43,4 +43,25 @@ class Solution463 {
         return islandPerimeter(grid, x - 1, y) + islandPerimeter(grid, x + 1, y) + islandPerimeter(grid, x, y - 1) + islandPerimeter(grid, x, y + 1);
     }
 
+    //    总周长 = 4 * 土地个数 - 2 * 接壤边的条数。
+    public int islandPerimeter1(int[][] grid) {
+        int count = 0, border = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    count++;
+//                    注意接壤的前提是本身也是陆地，所以判断要写在里面
+                    //                接壤的边只需要统计右和下即可，否则会重复统计
+                    if (i < grid.length - 1 && grid[i + 1][j] == 1) {
+                        border++;
+                    }
+                    if (j < grid[0].length - 1 && grid[i][j + 1] == 1) {
+                        border++;
+                    }
+                }
+            }
+        }
+        return count * 4 - border * 2;
+    }
+
 }
