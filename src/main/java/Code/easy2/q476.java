41f72334876ec3d4996776d9f60436c1adc842ff
chenjiale <2601823097@qq.com>
2023-07-20 22:24:46 +0800

    //    476. 数字的补数

---
+++ src/main/java/Code/easy2/q476.java
@@ -0,0 +1,53 @@
+package Code.easy2;
+
+/**
+ * @author 陈佳乐
+ * @create 2023-07-20-21:54
+ */
+public class q476 {
+    //    476. 数字的补数
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Solution476 {
+    //    java中没有同或运算符
+    public int findComplement(int num) {
+//        所以只能用最朴素的办法了
+        String string = Integer.toString(num, 2);
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == '0') {
+                res.append("1");
+            } else {
+                res.append("0");
+            }
+        }
+        return Integer.parseInt(res.toString(), 2);
+    }
+
+    //    大佬做法
+    public int findComplement1(int num) {
+//        找到最高位
+        int s = -1;
+        for (int i = 31; i >= 0; i--) {
+            if (((num >> i) & 1) != 0) {
+                s = i;
+                break;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < s; i++) {
+            if (((num >> i) & 1) == 0) ans |= (1 << i);
+        }
+        return ans;
+//   以下方法更简便
+//        int x = 0;
+//        for (int i = num; i != 0; i -= i & -i) x = i;
+//        return ~num & (x - 1);
+
+    }
+
+}
