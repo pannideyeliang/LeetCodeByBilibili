663b4648e48bad0511212c8aa0bdd4152e2828ec
chenjiale <2601823097@qq.com>
2023-07-23 20:05:23 +0800

    //    495. 提莫攻击

--- src/main/java/Code/easy2/q495.java
+++ src/main/java/Code/easy2/q495.java
@@ -18,10 +18,23 @@ class Solution495 {
                 res += duration;
             } else {
 //               否则直接就是看中毒时间间隔既可
-                res += timeSeries[i]-timeSeries[i-1];
+                res += timeSeries[i] - timeSeries[i - 1];
             }
             count = timeSeries[i] + duration - 1;
         }
         return res;
     }
+
+    //    查看大佬代码，
+//    想法一样，但是代码更简洁
+    public int findPoisonedDuration1(int[] timeSeries, int duration) {
+        int ans = 0, last = -1;
+        for (int s : timeSeries) {
+            int e = s + duration - 1;
+            ans += last < s ? duration : e - last;
+            last = e;
+        }
+        return ans;
+    }
+
 }
