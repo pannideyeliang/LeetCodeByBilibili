9db2a12c0723cefa4c951a6291135865b1137f37
chenjiale <2601823097@qq.com>
2023-07-23 18:23:10 +0800

    //492. 构造矩形

--- src/main/java/Code/easy2/q492.java
+++ src/main/java/Code/easy2/q492.java
@@ -26,6 +26,7 @@ class Solution492 {
     //    下面查看大佬的代码
 //    比较简洁：当正着来需要讨论的时候，那就直接反着来，直接从小的开始减
 //    反正小的不会超过大的，不需要额外讨论
+
     public int[] constructRectangle1(int area) {
         for (int i = (int) (Math.sqrt(area)); ; i--) {
             if (area % i == 0) return new int[]{area / i, i};
