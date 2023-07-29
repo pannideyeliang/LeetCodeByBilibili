255a3cdf73859e917c790e04c5258d82d48af536
chenjiale <2601823097@qq.com>
2023-07-29 21:39:23 +0800

    //    506. 相对名次，精简代码

--- src/main/java/Code/easy2/q506.java
+++ src/main/java/Code/easy2/q506.java
@@ -2,6 +2,7 @@ package Code.easy2;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class q506 {
     //    506. 相对名次
@@ -35,5 +36,23 @@ class Solution506 {
             res[i] = map.get(tmp[i]);
         }
         return res;
+    }
+
+    //    查看大佬的代码
+//    本题目的难点实际上就是如果记住每个数实际的大小顺序
+    public String[] findRelativeRanks1(int[] score) {
+        String[] ss = new String[]{"Gold Medal", "Silver Medal", "Bronze Medal"};
+        int n = score.length;
+        String[] ans = new String[n];
+        int[] clone = score.clone();
+        Arrays.sort(clone);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = n - 1; i >= 0; i--) map.put(clone[i], n - 1 - i);
+        for (int i = 0; i < n; i++) {
+//            利用这种方式做可以减少三个if else判断
+            int rank = map.get(score[i]);
+            ans[i] = rank < 3 ? ss[rank] : String.valueOf(rank + 1);
+        }
+        return ans;
     }
 }
