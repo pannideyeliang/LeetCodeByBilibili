ed2fcdd57883a95d94dc7e8c4afdebdb9a4bb496
chenjiale <2601823097@qq.com>
2024-05-27 10:27:06 +0800

    //    test

---
+++ src/main/java/Code/nbcb/test.java
@@ -0,0 +1,14 @@
+package Code.nbcb;
+
+/**
+ * @author 陈佳乐
+ * @create 2023-10-26-14:41
+ */
+public class test {
+    public static void main(String[] args) {
+        byte b = 4;
+        byte c = 9;
+        System.out.println(b+c);
+    }
+
+}
